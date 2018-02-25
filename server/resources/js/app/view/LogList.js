c41bca415bb93f2a7f9bca85b62686d4abef2ed5
rsteube <rsteube@users.noreply.github.com>
2018-02-25 16:37:18 +0100

    LogDetail as separate component

--- server/resources/js/app/view/LogList.js
+++ server/resources/js/app/view/LogList.js
@@ -1,47 +1,56 @@
 define(['app/models/Log'], function(Log) {
+  const color = {
+    'trace': '',
+    'debug': '',
+    'info': 'has-text-info',
+    'warn': 'has-text-warning',
+    'error': 'has-text-danger'
+  }
+
+  let LogDetail = {
+    view: function(logEntry) {
+      return m('table', {
+          class: 'table is-hoverable is-fullwidth'
+        },
+        Object.entries(logEntry.attrs).map(function([key, value]) {
+          return m('tr', [
+            m('td', key),
+            m('td', {
+              class: '' + color[value.toLowerCase()]
+            }, value)
+          ])
+        })
+      )
+    }
+  }
+
   return {
     activeEntry: "",
     oninit: Log.loadList,
     view: function(vnode) {
-      var color = {
-        'trace': '',
-        'debug': '',
-        'info': 'has-text-info',
-        'warn': 'has-text-warning',
-        'error': 'has-text-danger'
-      }
-
       return m('table', {
-          class: 'table is-hoverable is-fullwidth is-size-7'
-        },
-        [
+        class: 'table is-hoverable is-fullwidth is-size-7'
+      }, [
         m('thead',
-          m('tr',
-          [m('th'), m('th', '_timestamp'), m('th', '_msg')]
-          )
+          m('tr', [m('th'), m('th', '_timestamp'), m('th', '_msg')])
         ),
         m('tbody',
           Log.list.map(function(log) {
-              return m('tr', {onclick: function(){vnode.state.activeEntry = (vnode.state.activeEntry == log._timestamp ? "" : log._timestamp); m.redraw()}}, [
+            return m('tr', {
+              onclick: function() {
+                vnode.state.activeEntry = (vnode.state.activeEntry == log._timestamp ? "" : log._timestamp);
+                m.redraw()
+              }
+            }, [
               m('td', {
                 class: 'has-text-centered has-text-weight-bold ' + color[log._level.toLowerCase()]
-              }, log._level[0]), m('td', log._timestamp), 
-              
-              m('td', [log.msg,
-              m('table', {id: 'logDetail', class: 'table is-hoverable is-fullwidth ' + (vnode.state.activeEntry == log._timestamp? '' : 'is-hidden')},
-                Object.entries(log).map(function([key, value]) {
-                  return m('tr', [
-                    m('td', key),
-                    m('td', {class: ''+ color[value.toLowerCase()]}, value)
-                    ])
-                })
-              )])
-            
-            
+              }, log._level[0]), m('td', log._timestamp),
+
+              m('td', [log.msg, (vnode.state.activeEntry == log._timestamp ? m(LogDetail, log) : null)])
             ])
           })
-        )]
-      )
+        )
+      ])
     }
   }
 });
